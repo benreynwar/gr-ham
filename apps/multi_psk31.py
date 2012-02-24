2cfc853bff7c6c51b8bc4c371557a0a89aeacbe4
Ben Reynwar <ben@reynwar.net>
2012-02-23 22:42:22 -0700

    Improved multiple psk31 receiver.

--- apps/multi_psk31.py
+++ apps/multi_psk31.py
@@ -1,18 +1,17 @@
 import random, math, time, curses
 
 import ham
-from gnuradio import gr, window, digital
+from gnuradio import gr, window, digital, audio
 
-def get_ffted_source(src_factory, fftwidth, number_ffts=1):
+def get_ffted_source(src, fftwidth, number_ffts=1):
     """
     Get averaged fft data for the source.
 
-    `src_factory` is a function that generates a source block.
+    `src` is the source block.
     `fftwidth` is the width of the fft.
     `number_ffts` is the number of ffts that should be averaged together.
     """
     tb = gr.top_block()
-    src = src_factory()
     stream_to_vector = gr.stream_to_vector(gr.sizeof_float, fftwidth)
     fft = gr.fft_vfc(fftwidth, True, window.blackmanharris(fftwidth))
     vector_to_stream = gr.vector_to_stream(gr.sizeof_gr_complex, fftwidth)
@@ -74,48 +73,56 @@ def get_peaks(data, samp_rate, fftwidth=1000, cutoff=100):
                 peak_freqs.append(f)
     return peak_freqs
 
-class psk31_topblock(gr.top_block):
+class psk31_stream(object):
     """
-    A top block that takes care of psk31 receiving.
+    Represents the section of a flow graph that receives a psk31 signal.
     """
 
-    def __init__(self, src_factory, carrier_freq, symbol_rate=31.25,
-                 samp_rate=44100, decim=200, slowdown=1):
-        gr.top_block.__init__(self)
-        self.symbol_rate = symbol_rate
-        self.samp_rate = samp_rate
-        self.decim = decim 
+    def __init__(self, tb, carrier_freq, samp_rate, symbol_rate=31.25, decim=200):
+        self.tb = tb
         self.carrier_freq = carrier_freq
+        self.symbol_rate = symbol_rate
+        self.decim = decim
+        self.samp_rate = samp_rate
 
         self.msgq_out = gr.msg_queue()
-
-        src = src_factory()
-        decoder = ham.psk31_decode_bb(True)
-        null = gr.null_source(gr.sizeof_float)
-        float_to_complex = gr.float_to_complex(1)
-        coswave = gr.sig_source_c(self.samp_rate, gr.GR_COS_WAVE,
+        self.decoder = ham.psk31_decode_bb(True)
+        self.null = gr.null_source(gr.sizeof_float)
+        self.float_to_complex = gr.float_to_complex(1)
+        self.coswave = gr.sig_source_c(self.samp_rate, gr.GR_COS_WAVE,
                                   -self.carrier_freq, 1, 0)
-        multiply = gr.multiply_vcc(1)
-	snk = gr.message_sink(gr.sizeof_char, self.msgq_out, True)
+        self.multiply = gr.multiply_vcc(1)
+	self.snk = gr.message_sink(gr.sizeof_char, self.msgq_out, True)
         midpoint = 60
         width = 20
         n_taps = 10
         taps = gr.firdes.low_pass_2(decim, samp_rate, midpoint, width, n_taps)
-	lpf = gr.fft_filter_ccc(decim, taps)
-	diff = gr.diff_decoder_bb(2)
-	costas = digital.costas_loop_cc(2*3.14/100, 4)
-	receiver = digital.constellation_receiver_cb(
+	self.lpf = gr.fft_filter_ccc(decim, taps)
+	self.diff = gr.diff_decoder_bb(2)
+	self.costas = digital.costas_loop_cc(2*3.14/100, 4)
+	self.receiver = digital.constellation_receiver_cb(
             digital.constellation_bpsk().base(), 2*3.14/100, -0.25, 0.25)
-        clock_recovery = digital.clock_recovery_mm_cc(
+        self.clock_recovery = digital.clock_recovery_mm_cc(
             samp_rate/decim/symbol_rate, 0.25 * 0.1*0.1, 0.05, 0.1, 0.001)
-        throttle = gr.throttle(gr.sizeof_float, samp_rate/slowdown)
-
-        self.connect(src, throttle, (float_to_complex, 0))
-        self.connect(null, (float_to_complex, 1))
-        self.connect(float_to_complex, (multiply, 0))
-        self.connect(coswave, (multiply, 1))
-        self.connect(multiply, lpf, costas, clock_recovery, receiver, diff, decoder, snk)
         self.message = ''
+        self.tb.connect(self.null, (self.float_to_complex, 1))
+        self.tb.connect(self.float_to_complex, (self.multiply, 0))
+        self.tb.connect(self.coswave, (self.multiply, 1))
+        self.tb.connect(self.multiply, self.lpf, self.costas, self.clock_recovery,
+                   self.receiver, self.diff, self.decoder, self.snk)
+        # Number of times we've not seen frequency in peaks.
+        self.consecutive_silences = 0
+        self.active = False
+
+    def connect(self):
+        """Connect this section into the top block."""
+        self.tb.connect(self.tb.src, (self.float_to_complex, 0))
+        self.active = True
+        
+    def disconnect(self):
+        """Disconnect this section into the top block."""
+        self.tb.disconnect(self.tb.src, (self.float_to_complex, 0))
+        self.active = False
 
     def get_message(self):
         """
@@ -123,6 +130,21 @@ class psk31_topblock(gr.top_block):
         """
         self.message += msgq_to_string(self.msgq_out)
         return self.message
+
+class psk31_topblock(gr.top_block):
+    """
+    A top block that just sets up the source.
+    psk31_stream objects make the rest of the connections.
+    """
+
+    def __init__(self, src, throttle=None):
+        gr.top_block.__init__(self)
+        if throttle is None:
+            self.src = src
+        else:
+            self.real_src = src
+            self.src = throttle
+            self.connect(src, throttle)
 
 def msgq_to_string(q):
     """
@@ -136,69 +158,129 @@ def msgq_to_string(q):
     # Replace carriage returns by line feeds.
     new_msg = []
     for c in all_msg:
-        if ord(c) == 13:
+        if ord(c) in (10, 13):
             new_msg.append('\n')
+        elif (ord(c) < 32):
+            new_msg.append('?')
         else:
             new_msg.append(c)
     return ''.join(new_msg)
+
+def update_steams(freqs, streams, tb, samp_rate):
+    """
+    Connect, disconnect and create new psk31 streams based on
+    the observed peak frequencies.
+    """
+    delta_freq = 20
+    consec_silences = 5
+    found_streams = []
+    new_freqs = []
+    for freq in freqs:
+        found = False
+        for stream in streams:
+            if abs(freq - stream.carrier_freq) < delta_freq:
+                found_streams.append(stream)
+                found = True
+                break
+        if not found:
+            new_freqs.append(freq)
+    silent_streams = list(set(streams) - set(found_streams))
+    for s in silent_streams:
+        s.consecutive_silences += 1
+        if s.consecutive_silences > consec_silences:
+            s.disconnect()
+    for s in found_streams:
+        s.consecutive_silences = 0
+        if not s.active:
+            s.connect()
+    new_streams = [psk31_stream(tb, freq, samp_rate) for freq in new_freqs]
+    for s in new_streams:
+        s.connect()
+    return streams + new_streams
+        
     
-def decode(stdscr, src_factory, samp_rate, fftwidth, cutoff, total_time=10):
+def decode(stdscr, src, samp_rate, fftwidth, cutoff, total_time=10, time_step=0.1, scan_time=1):
     """
     Given a function that generates the source this will:
      - detect the peaks.
      - create a receiver for each.
      - write the results to the terminal (using curses module).
     """
-    ffted = get_ffted_source(src_factory, fftwidth, 100)
+    ffted = get_ffted_source(src, fftwidth, 100)
     peak_freqs = get_peaks(ffted, samp_rate, fftwidth, cutoff)
-    signals = [psk31_topblock(src_factory, freq, slowdown=1) for freq in peak_freqs]
-    for s in signals:
-        s.start()
-    time_step = 0.1
-    for i in range(0, int(total_time/time_step)):
+    tb = psk31_topblock(src)
+    streams = []
+    streams = update_steams(peak_freqs, streams, tb, samp_rate)
+    tb.start()
+    scan_steps = int(scan_time/time_step)
+    for t in range(0, int(total_time/time_step)):
+        #if (t+1) % scan_steps == 0:
+        #    # Need calculation of peak freqs here
+        #    tb.lock()
+        #    streams = update_steams(peak_freqs, streams, tb, samp_rate)
+        #    tb.unlock()
         spacing = 4
         stdscr.erase()
-        for i, s in enumerate(signals):
-            stdscr.addstr(i*spacing, 0,
+        for i, s in enumerate(streams):
+            stdscr.addstr(0, 0, str(t*time_step))
+            stdscr.addstr(i*spacing+1, 0,
                           '**{0:0.2f}*****'.format(s.carrier_freq))
             m = s.get_message()
             lines = m.split('\n')
             n_lines = len(lines)
             if n_lines > spacing-2:
                 m = '\n'.join(lines[n_lines-spacing+2:])
-            stdscr.addstr(i*spacing+1, 0, m)
+            stdscr.addstr(i*spacing+2, 0, m)
         stdscr.refresh()
         time.sleep(time_step)
-    for s in signals:
-        s.stop()
+    tb.stop()
 
-def make_src():
-    return gr.wavfile_source('example.WAV', True) 
+class fake_stdscr(object):
+    """
+    A fake stdscr object for debugging.
+    (curses is a pain to debug with).
+    """
+    def addstr(self, x, y, text):
+        print(text)
+    def erase(self):
+        print('-----------------')
+    def refresh(self):
+        pass
 
-def start():
-    """Set up curses."""
-    stdscr = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-    stdscr.keypad(1)
+def start(fake=False):
+    """
+    Set up curses.
+    If `fake` is True then it doesn't really use curses. 
+    """
+    if fake:
+        stdscr = fake_stdscr()
+    else:
+        stdscr = curses.initscr()
+        curses.noecho()
+        curses.cbreak()
+        stdscr.keypad(1)
     return stdscr
 
-def end(stdscr):
+def end(stdscr, fake=False):
     """Break down curses."""
-    curses.nocbreak()
-    stdscr.keypad(0)
-    curses.echo()
-    curses.endwin()
+    if not fake:
+        curses.nocbreak()
+        stdscr.keypad(0)
+        curses.echo()
+        curses.endwin()
     
 def execute(stdscr):
     samp_rate = 44100
-    decode(stdscr, make_src, samp_rate, 1000, 100, total_time=100)
+    #src = audio.source(44100, "", True)
+    src = gr.wavfile_source("example2.wav", False)
+    decode(stdscr, src, samp_rate, 1000, 100, total_time=10, time_step=1)
 
 if __name__ == '__main__':
-    stdscr = start()
+    fake = False
+    stdscr = start(fake)
     try:
         execute(stdscr)
     except:
-        end(stdscr)
+        end(stdscr, fake)
         raise
-    end(stdscr)
+    end(stdscr, fake)
